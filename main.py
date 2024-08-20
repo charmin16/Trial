10bd629858997379b09d86ff3b54ca8b93411677
charmin16 <charming_princewill@yahoo.com>
2024-08-20 22:36:26 +0100

    Dear Mr President

---
+++ main.py
@@ -0,0 +1,7 @@
+names = ['Nelson', 'Dami', 'Charming']
+country_capital = {'Nigeria': 'Abuja',
+                   'Ghana': 'Accra',
+                   'Liberia': 'Monrovia'}
+
+print(names)
+print(country_capital)
---
+++ main1.py
@@ -0,0 +1,7 @@
+names = ['Ibiba', 'Francis', 'Alaso']
+country_capital = {'Brazil': 'Brazilia',
+                   'Britain': 'London',
+                   'Sweden': 'Berlin'}
+
+print(names)
+print(country_capital)
